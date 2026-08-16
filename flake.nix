b4f8f7ac3c9ed0f9b8e2dcee16a392980526f27b
Orry Jarvis <orryjarvis@gmail.com>
2026-08-16 02:51:11 +0000

    init repo

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      formatter.${system} = pkgs.nixfmt;
+
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          zig
+          zls
+
+          nixd
+          nixfmt
+
+          lldb
+        ];
+      };
+    };
+}
